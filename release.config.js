913607fda71f88e08e4d3fd9a8dec113f529136c
oleg-konyk <oleg@gomerits.com>
2023-09-19 17:20:53 -0400

    feat: test 21

---
+++ release.config.js
@@ -0,0 +1,27 @@
+const child_process = require("child_process");
+
+const branchName = child_process.execSync('git rev-parse --abbrev-ref HEAD');
+const sanitizedBranchName = branchName.includes("feat/") ? branchName.replace("feat/", "") : branchName.replace("/", "");
+
+console.log(result.toString())
+
+module.exports = {
+  "branches": [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "main",
+    {name: "beta", prerelease: true},
+    {name: "alpha", prerelease: true},
+    { name: "feat/**", prerelease:  sanitizedBranchName}
+  ],
+  "plugins": [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        "preset": "conventionalcommits"
+      }
+    ],
+    "@semantic-release/github",
+  ]
+}
+
+
