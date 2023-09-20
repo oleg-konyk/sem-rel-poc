6a5fe9802a894f887a4856bf263e5a3e00dd42c2
oleg-konyk <oleg@gomerits.com>
2023-09-20 10:58:23 -0400

    feat: test 60

--- release.config.js
+++ release.config.js
@@ -1,15 +1,23 @@
 const child_process = require("child_process");
 
+/**
+ * to handle feature branches in format of "feat/org-1234-myfeature"
+ * we need to sanitize branch name so created tag conforms to https://semver.org/#spec-item-9
+ */
 const handleFeatureBranch = () => {
+  const featureBranchPreFix = "feat/";
+  const printBranchCmd = "git branch --show-current";
   const invalidCharacters = /[&\/\\#,+()$~%_'":*?<>{}]/g;
-  const rawBranchName = child_process.execSync('git branch --show-current')
+  const rawBranchName = child_process.execSync(printBranchCmd)
     .toString()
     .trim();
   const sanitizedBranchName = rawBranchName
     .replace(invalidCharacters, "-")
     .trim();
 
-    if (rawBranchName.includes("feat/")) {
+    if (rawBranchName.includes(featureBranchPreFix)) {
+      // each feat branch needs to be pushed one at a time to keep "prerelease" values unique
+      // making "name" a glob pattern is not compatible with dynamic "prerelease" string calculation
       return [{ name: rawBranchName, prerelease: sanitizedBranchName }];
     }
 
